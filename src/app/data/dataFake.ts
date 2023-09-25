2c488878ba04b0fad45aed19ed9106f9314624b6
Flávio Peres <flaviopc2@gmail.com>
2023-09-25 19:12:14 -0300

    Primeira parte do meu blog está concluída

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photocover": "https://conteudo.imguol.com.br/c/entretenimento/fa/2023/09/22/a-fazenda-2023-rachel-sheherazade-chama-tonzao-de-sexista-1695358492315_v2_900x506.png.webp",
+        "title": "Como Rachel Sheherazade pode arruinar A Fazenda 15",
+        "description": "Começou animada a 15ª temporada de A Fazenda. Com menos de uma semana no ar, já aconteceu uma miríade de fortes emoções com aquele tradicional leque de intrigas, conchavos e devaneios. Alguns participantes começaram muito bem, despontando como possíveis favoritos, caso de Márcia Fu, apenas para alguns dias depois perderem o apoio popular de maneira contundente…"
+    },
+    {
+        "id": "2",
+        "photocover": "https://conteudo.imguol.com.br/c/splash/a8/2021/02/20/sandy-e-lucas-lima-lancam-versao-de-lugar-ao-sol-1613864554538_v2_300x225.jpg.webp",
+        "title": "Sandy e Lucas Lima anunciam fim de casamento: Não foi uma decisão fácil",
+        "description": "Após 24 anos de relacionamento, Sandy e Lucas Lima não estão mais casados. Os dois anunciaram o término num comunicado conjunto nas redes sociaisNão foi uma decisão fácil, nem impulsiva. Foram praticamente 24 anos de relacionamento e 15 anos de casados. Com altos e baixos, às vezes mais felizes, às vezes menos, mas sempre inteiros e dispostos a fazer o nosso melhor. E fizemos.Não teve briga, mágoa, traumas... a gente conseguiu enxergar que esse era o melhor caminho e vamos deixar de ser um casal do mesmo jeito que a gente foi um: com muito amor, respeito e amizade infinita"
+    },
+    {
+        "id": "3",
+        "photocover": "https://conteudo.imguol.com.br/c/home/d1/2023/09/25/250923---o-presidente-lula-pt-em-entrevista-no-itamaraty-1695668543769_300x225.jpg.webp",
+        "title": "Lula nega angústia e descarta raça e gênero como critérios para STF",
+        "description": "O presidente Lula (PT) descartou hoje que gênero e raça serão critérios para a indicação do próximo ministro do STF (Supremo Tribunal Federal) e disse não ter angústia para divulgar o novo nome. O que Lula disse O presidente afirmou já ter várias pessoas na mira, mas que não precisava ser questionado sobre raça ou gênero. Lula tem sido pressionado por apoiadores e setores progressistas para indicar uma mulher na vaga da ministra Rosa Weber, que se aposenta em 2 de outubro…"
+    },
+    {
+        "id": "4",
+        "photocover": "https://conteudo.imguol.com.br/c/noticias/86/2023/07/12/ciclone-extratropical-na-costa-do-brasil-1689173943857_v2_300x225.png.webp",
+        "title": "Ciclone se forma no Rio Grande do Sul e frente fria avança na região; veja previsões",
+        "description": "A formação de um novo ciclone extratropical em alto-mar próximo à costa de Santa Catarina e o litoral norte do Rio Grande do Sul entre a noite de terça-feira, 26, e a madrugada quarta, 27, vai intensificar a instabilidade na região com muita chuva, vento e tempo severo, conforme a Defesa Civil gaúcha.O órgão acrescenta, no entanto, que a intensificação das chuvas será devido ao aprofundamento e avanço de um sistema de baixa pressão para o oceano, fluxo de umidade do oceano combinado ao da Amazônia e formação de uma frente fria.Nesta segunda-feira, 25, fortes áreas de instabilidade já se formam no território gaúcho ainda pela manhã, principalmente sobre Passo Fundo, Erechim, Cax…"
+    }
+]
